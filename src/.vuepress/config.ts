93122141b0872600d3b4f13306bcd9c70869f7a7
Wen Bin <a1231512a@163.com>
2024-10-04 10:32:12 +0800

    修复github page推送问题

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -3,7 +3,7 @@ import {defineUserConfig} from "vuepress";
 import theme from "./theme.js";
 
 export default defineUserConfig({
-    base: "/",
+    base: "/note-vuepress/",
 
     lang: "zh-CN",
     title: "博客演示",
--- src/.vuepress/theme.ts
+++ src/.vuepress/theme.ts
@@ -91,8 +91,6 @@ export default hopeTheme({
   plugins: {
     blog: true,
 
-    git: true,
-
     // 启用之前需安装 @waline/client
     // 警告: 这是一个仅供演示的测试服务，在生产环境中请自行部署并使用自己的服务！
     // comment: {
